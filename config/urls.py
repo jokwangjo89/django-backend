a802eea06c6ae657eeb950006368181671d22208
jokwangjo89 <samgugjojo@gmail.com>
2025-01-06 09:30:56 +0900

    update

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the homepage!")
 
 urlpatterns = [
+    path('', home),
     path('mqtt/', include('mqtt.urls')),
     path('admin/', admin.site.urls),
 ]
